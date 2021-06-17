3836755bde5d6eca25fcaf6ec0f047a9e211e373
bnbabu2142 <bnbabu2142@gmail.com>
2021-06-17 16:47:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mat-table-with-api-call-skexjs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mat-table-with-api-call-skexjs)
---
+++ app/table-basic-example.ts
@@ -0,0 +1,37 @@
+import {Component} from '@angular/core';
+import {APIServiceService } from './api-service.service';
+
+export interface Data {
+  userId: string;
+  Id: number;
+  title: number;
+  completed: boolean;
+}
+
+const ELEMENT_DATA: Data[] = [
+];
+
+/**
+ * @title Basic use of `<table mat-table>`
+ */
+@Component({
+  selector: 'table-basic-example',
+  styleUrls: ['table-basic-example.css'],
+  templateUrl: 'table-basic-example.html',
+})
+export class TableBasicExample {
+  displayedColumns: string[] = ['id', 'userId', 'title', 'completed'];
+  
+  dataSource : any[] = [];
+
+  constructor(private service : APIServiceService){
+    this.service.getData().then(data => {
+      this.dataSource = data;
+    });
+  }
+}
+
+
+/**  Copyright 2019 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
